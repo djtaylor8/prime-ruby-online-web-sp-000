6104003b484257b9635d5ee484d51785aa6c10b2
DJ Taylor <dtaylor88@gmail.com>
2021-06-01 04:01:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,7 @@
 def prime?(num)
-  #if given 0 ornegative number, return false
   return false if num <= 0 
-  #create range of numbers from 2 to one minus number
   (2..(num - 1)).each do |n|
-    #if any number in this range is evenly divided, return false
     return false if num % n == 0
   end
-  #return true if no tests pass above
   true
 end
